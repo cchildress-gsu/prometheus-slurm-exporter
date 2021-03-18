5fd1856aa230e4e9f0be590ba11828ce2057c7b2
Chris Childress <cchildress@gsu.edu>
2021-03-18 14:27:37 -0400

    added normusage to sshare stats

--- sshare.go
+++ sshare.go
@@ -16,71 +16,80 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 package main
 
 import (
-        "io/ioutil"
-        "os/exec"
-        "log"
-        "strings"
-        "strconv"
-        "github.com/prometheus/client_golang/prometheus"
+	"io/ioutil"
+	"log"
+	"os/exec"
+	"strconv"
+	"strings"
+
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 func FairShareData() []byte {
-        cmd := exec.Command( "sshare", "-n", "-P", "-o", "account,fairshare" )
-        stdout, err := cmd.StdoutPipe()
-        if err != nil {
-                log.Fatal(err)
-        }
-        if err := cmd.Start(); err != nil {
-                log.Fatal(err)
-        }
-        out, _ := ioutil.ReadAll(stdout)
-        if err := cmd.Wait(); err != nil {
-                log.Fatal(err)
-        }
-        return out
+	cmd := exec.Command("sshare", "-n", "-P", "-o", "account,fairshare,normusage")
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+	out, _ := ioutil.ReadAll(stdout)
+	if err := cmd.Wait(); err != nil {
+		log.Fatal(err)
+	}
+	return out
 }
 
 type FairShareMetrics struct {
-        fairshare float64
+	fairshare float64
+	normusage float64
 }
 
 func ParseFairShareMetrics() map[string]*FairShareMetrics {
-        accounts := make(map[string]*FairShareMetrics)
-        lines := strings.Split(string(FairShareData()), "\n")
-        for _, line := range lines {
-                if ! strings.HasPrefix(line,"  ") {
-                        if strings.Contains(line,"|") {
-                                account := strings.Trim(strings.Split(line,"|")[0]," ")
-                                _,key := accounts[account]
-                                if !key {
-                                        accounts[account] = &FairShareMetrics{0}
-                                }
-                                fairshare,_ := strconv.ParseFloat(strings.Split(line,"|")[1],64)
-                                accounts[account].fairshare = fairshare
-                        }
-                }
-        }
-        return accounts
+	accounts := make(map[string]*FairShareMetrics)
+	lines := strings.Split(string(FairShareData()), "\n")
+	for _, line := range lines {
+		if strings.HasPrefix(line, "  ") {
+			if strings.Contains(line, "|") {
+				line_split := strings.Split(line, "|")
+				account := strings.Trim(line_split[0], " ")
+				_, key := accounts[account]
+				if !key {
+					accounts[account] = &FairShareMetrics{0, 0}
+				}
+				fairshare, _ := strconv.ParseFloat(line_split[1], 64)
+				normusage, _ := strconv.ParseFloat(line_split[2], 64)
+				accounts[account].fairshare = fairshare
+				accounts[account].normusage = normusage
+			}
+		}
+	}
+	return accounts
 }
 
 type FairShareCollector struct {
-        fairshare *prometheus.Desc
+	fairshare *prometheus.Desc
+	normusage *prometheus.Desc
 }
 
 func NewFairShareCollector() *FairShareCollector {
-        labels := []string{"account"}
-        return &FairShareCollector{
-                fairshare: prometheus.NewDesc("slurm_account_fairshare","FairShare for account" , labels,nil),
-        }
+	labels := []string{"account"}
+	return &FairShareCollector{
+		fairshare: prometheus.NewDesc("slurm_account_fairshare", "FairShare for account", labels, nil),
+		normusage: prometheus.NewDesc("slurm_account_normusage", "Normalized usage for account", labels, nil),
+	}
 }
 
 func (fsc *FairShareCollector) Describe(ch chan<- *prometheus.Desc) {
-        ch <- fsc.fairshare
+	ch <- fsc.fairshare
+	ch <- fsc.normusage
 }
 
 func (fsc *FairShareCollector) Collect(ch chan<- prometheus.Metric) {
-        fsm := ParseFairShareMetrics()
-        for f := range fsm {
-                ch <- prometheus.MustNewConstMetric(fsc.fairshare, prometheus.GaugeValue, fsm[f].fairshare, f)
-        }
+	fsm := ParseFairShareMetrics()
+	for f := range fsm {
+		ch <- prometheus.MustNewConstMetric(fsc.fairshare, prometheus.GaugeValue, fsm[f].fairshare, f)
+		ch <- prometheus.MustNewConstMetric(fsc.normusage, prometheus.GaugeValue, fsm[f].normusage, f)
+	}
 }
